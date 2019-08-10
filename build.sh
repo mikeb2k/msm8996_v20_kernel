6c387c6dc29a7c99eb50ccc6335872f57a376e32
Michael Bardoň <wolfovelp@gmail.com>
2019-08-10 16:01:43 +0200

    test

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 export KBUILD_BUILD_USER=w0lf
-export KBUILD_BUILD_HOST=Maximus
+export KBUILD_BUILD_HOST=Acer
 export ARCH=arm64
-export CROSS_COMPILE=/home/michael/v20/gcc/bin/aarch64-linux-android-
+export CROSS_COMPILE=/home/michael/toolchains/gcc_4_9/bin/aarch64-linux-android-
 export CONFIG_NO_ERROR_ON_MISMATCH=y
 
 DIR=$(pwd)
@@ -24,10 +24,10 @@ make O="$BUILD" elsa_global_com-perf_defconfig
 echo "building kernel..."
 time make O="$BUILD" -j$NPR 2>&1 |tee ../compile.log
 
-#echo "building modules..."
-#make O="$BUILD" INSTALL_MOD_PATH="." INSTALL_MOD_STRIP=1 modules_install
-#rm $BUILD/lib/modules/*/build
-#rm $BUILD/lib/modules/*/source
+echo "building modules..."
+make O="$BUILD" INSTALL_MOD_PATH="." INSTALL_MOD_STRIP=1 modules_install
+rm $BUILD/lib/modules/*/build
+rm $BUILD/lib/modules/*/source
 
 mkdir -p $OUT/modules
 mv "$BUILD/arch/arm64/boot/Image.gz-dtb" "$OUT/Image.gz-dtb"
